8669414f5c35747f3b771ff50d00366250177d80
prateekagrawal589-create <prateekagrawal589@gmail.com>
2026-09-10 13:53:03 +0530

    Update connect.routes.js

--- backend/src/routes/connect.routes.js
+++ backend/src/routes/connect.routes.js
@@ -5,7 +5,9 @@ const { requireAuth } = require('../middleware/auth');
 
 router.post('/request', requireAuth, c.sendRequest);
 router.put('/:id/respond', requireAuth, c.respondRequest);
+router.delete('/:id', requireAuth, c.removeConnection);
 router.get('/my', requireAuth, c.getMyConnections);
 router.get('/pending', requireAuth, c.getPendingRequests);
+router.get('/sent', requireAuth, c.getSentRequests);
 
 module.exports = router;
